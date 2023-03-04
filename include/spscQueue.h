212fa4b7d2601c7fe3fee1798f27ada495bf2d11
Mac Mleczko <mlemacio0@gmail.com>
2023-03-03 19:17:44 -0600

    A base C++ directory

---
+++ include/spscQueue.h
@@ -0,0 +1,13 @@
+#ifndef INCLUDE_SPSC_QUEUE_H
+#define INCLUDE_SPSC_QUEUE_H
+
+namespace spsc
+{
+    class spscQueue_t
+    {
+    public:
+        explicit spscQueue_t() {}
+    };
+};
+
+#endif // INCLUDE_SPSC_QUEUE_H
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "include/spscQueue.h"
+
+auto main() -> int {
+    spsc::spscQueue_t q;
+    return 0;
+}
