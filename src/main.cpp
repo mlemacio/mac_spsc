00a3c31f75c0d873ab0b94224b06cca4ab45ebcc
Mac Mleczko <mlemacio0@gmail.com>
2023-06-25 14:47:46 -0500

    This segfaults... why?

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,29 @@
 
 auto main() -> int
 {
-    static constexpr size_t CAPACITY = 10;
+    {
+        static constexpr size_t CAPACITY = 10;
 
-    spsc::spscQueue_t<int> q(CAPACITY);
+        spsc::spscQueue_t<std::vector<int>> q(CAPACITY);
 
-    q.emplace(12);
+        for (size_t i{}; i < 3; i++)
+        {
+            q.emplace(std::vector<int>{1, 2, 3});
+        }
 
-    std::cout << q.front().value() << std::endl;
+        std::cout << std::boolalpha;
+
+        auto maybeVal = q.front();
+        while (maybeVal.has_value())
+        {
+            for (const auto &v : maybeVal.value().get())
+            {
+                std::cout << v << std::endl;
+            }
+            q.pop();
+            maybeVal = q.front();
+        }
+    }
 
     return 0;
 }
